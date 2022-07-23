82ba1cc4cab9ec8add4c6de7b9649128bd604ddd
Adnan-5 <sahilladnan810@gmail.com>
2022-07-23 12:25:49 +0500

    list elements added in ci=onstractor function

--- list/list.js
+++ list/list.js
@@ -1,4 +1,13 @@
 function List() {
     this.listSize=0;
-    this.pos=0
+    this.pos=0;
+    this.dataStore = [];
+    this.clear = clear;
+    this.find = find;
+    this.append = append;
+     this.toString = toString;
+     this.remove = remove;
+     this.length = length;
 }
+
+// length of the list
