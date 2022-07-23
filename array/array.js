ded3b514e06aa2e9344bacf9914373ab989da61b
Adnan-5 <sahilladnan810@gmail.com>
2022-07-23 11:44:22 +0500

    function added

--- array/array.js
+++ array/array.js
@@ -5,3 +5,9 @@ var positiveNumbers = [1,2,3,4,5];
 var negativeNumbers = [-1,-2,-3,-4,-5]
 var numbers = negativeNumbers.concat(zero,positiveNumbers); // concatinating two or more strings
 console.log(numbers);
+
+
+var isEven = function(x){
+    // return true if x is multiple of 2
+    console.log(x);
+}
