da25c7af5a2f5793d22f417161580f5ed6638e78
Adnan-5 <sahilladnan810@gmail.com>
2022-07-23 11:57:52 +0500

    getting the array elements by using loop

--- array/twoDimentionalArray.js
+++ array/twoDimentionalArray.js
@@ -7,3 +7,13 @@ var averageTemp = [];
 averageTemp[0] = [72,75,79,79,81,81];
 averageTemp[1] = [81,79,75,75,73,72];
 console.log(averageTemp);
+
+function printMatrix(myMatrix){
+    for(var i=0; i<myMatrix.length; i++){
+        for(var j=0; j<myMatrix[i].length;j++){
+            console.log(myMatrix[i][j]);
+        }
+    }
+}
+
+printMatrix(averageTemp);
