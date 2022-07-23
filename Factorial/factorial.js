be43bd422206c2224c84004e299a924490b59c83
Adnan-5 <sahilladnan810@gmail.com>
2022-07-23 19:40:38 +0500

    constractor function Stack created

---
+++ Factorial/factorial.js
@@ -0,0 +1,18 @@
+function Stack() {
+    this.dataStore = []
+    this.top = 0;
+    this.push= push;
+    this.pop = pop;
+    this.peek = peek;
+    this.clear = clear;
+   this.length = length;
+}
+
+// adding the element   in Stack
+function push(element) {
+    this.dataStore[this.top++]= element;
+}
+
+function peek() {
+    return this.dataStore[this.top-1];
+}
