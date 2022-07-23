84e9723b26468748964078365be4878f1cd4a4f6
Adnan-5 <sahilladnan810@gmail.com>
2022-07-23 20:05:42 +0500

    arrange  the code

--- Factorial/factorial.js
+++ Factorial/factorial.js
@@ -1,42 +1,42 @@
 function Stack() {
-    this.dataStore = []
-    this.top = 0;
-    this.push= push;
-    this.pop = pop;
-    this.peek = peek;
-    this.clear = clear;
-   this.length = length;
+  this.dataStore = [];
+  this.top = 0;
+  this.push = push;
+  this.pop = pop;
+  this.peek = peek;
+  this.clear = clear;
+  this.length = length;
 }
 
 // adding the element   in Stack
 function push(element) {
-    this.dataStore[this.top++]= element;
+  this.dataStore[this.top++] = element;
 }
 
 function peek() {
-    return this.dataStore[this.top-1];
+  return this.dataStore[this.top - 1];
 }
 // for removing
-function pop(){
-    return this.dataStore[--this.top];
+function pop() {
+  return this.dataStore[--this.top];
 }
 function clear() {
-    this.top= 0;
+  this.top = 0;
 }
- function length(){
-    return this.top;
- }
+function length() {
+  return this.top;
+}
 // factorial fnction goes here
-function factorial(n){
-var stack = new Stack();
-while (n>1) {
+function factorial(n) {
+  var stack = new Stack();
+  while (n > 1) {
     stack.push(n--);
-}
-var prod = 1;
-while (stack.length()> 0) {
-     prod *= stack.pop(); // product as 5*4*3*2*1
-}
-return prod
+  }
+  var prod = 1;
+  while (stack.length() > 0) {
+    prod *= stack.pop(); // product as 5*4*3*2*1
+  }
+  return prod;
 }
 
 console.log("factorial of 5 is :" + factorial(5));
