74ae8b9260e475c0889500a85af733fd8488dd28
Adnan-5 <sahilladnan810@gmail.com>
2022-07-23 11:36:44 +0500

    concatinate

--- array/array.js
+++ array/array.js
@@ -3,3 +3,4 @@
 var zero = 0;
 var positiveNumbers = [1,2,3,4,5];
 var negativeNumbers = [-1,-2,-3,-4,-5]
+var numbers = negativeNumbers.concat(zero,positiveNumbers); // concatinating two or more strings
