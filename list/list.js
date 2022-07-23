85b149100f01ad207c66a76c8df7c97bb8bc5d56
Adnan-5 <sahilladnan810@gmail.com>
2022-07-23 12:40:00 +0500

    removing an element from the list

--- list/list.js
+++ list/list.js
@@ -31,3 +31,15 @@ function find(element) {
     }
     return -1;
 }
+
+// Removing an element
+
+function remove(element) {
+    var foundAt = this.find(element);
+    if (foundAt>-1) {
+        this.dataStore.splice(foundAt,1);
+        --this.listSize;
+        return true;
+    }
+    return false;
+}
