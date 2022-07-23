2a95190e44cc209cf21cb6ea98aebf846c800f42
Adnan-5 <sahilladnan810@gmail.com>
2022-07-23 20:25:19 +0500

    stack constructor fun created

---
+++ baseConversion/baseConversion.js
@@ -0,0 +1,30 @@
+function Stack() {
+    this.dataStore = []
+    this.top = 0;
+    this.push= push;
+    this.pop = pop;
+    this.peek = peek;
+    this.clear = clear;
+   this.length = length;
+}
+
+// adding the element   in Stack
+function push(element) {
+    this.dataStore[this.top++]= element;
+}
+
+function peek() {
+    return this.dataStore[this.top-1];
+}
+// removing the element 
+function pop() {
+    return this.dataStore[--this.top];
+}
+function clear() {
+    this.top = 0;
+}
+
+// Returning the length of the Stack
+function length(){
+   return this.top;
+}
