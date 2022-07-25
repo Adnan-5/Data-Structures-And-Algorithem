e92ab042b2c1e274b71a77622c09fdce95de43df
Adnan-5 <sahilladnan810@gmail.com>
2022-07-25 12:07:27 +0000

    assign new values and accessing

--- baseConversion/baseConversion.js
+++ baseConversion/baseConversion.js
@@ -48,3 +48,7 @@ function mulBase(num, base) {
     var base = 2;
     var newNum = mulBase(num, base);
    console.log(num + " converted to base " + base + " is " + newNum);
+    num = 125;
+    base = 8;
+    var newNum = mulBase(num, base);
+   console.log(num + " converted to base " + base + " is " + newNum);
