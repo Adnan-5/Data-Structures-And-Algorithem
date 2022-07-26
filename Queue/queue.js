3a970336d54d0092676ba81ac2c010f34dfa4537
Adnan-5 <sahilladnan810@gmail.com>
2022-07-26 17:26:47 +0500

    Queue class by defining the constructor function

---
+++ Queue/queue.js
@@ -0,0 +1,9 @@
+function Queue() {
+    this.dataStore = [];
+    this.enqueue = enqueue;
+    this.dequeue = dequeue;
+    this.front = front;
+    this.back = back;
+    this.toString = toString;
+    this.empty = empty;
+    }
