ef5ade6a0f43775e32cc36bed69e8150ec803395
Adnan-5 <sahilladnan810@gmail.com>
2022-07-26 18:28:01 +0500

    arrange code in  prettier

--- Queue/queue.js
+++ Queue/queue.js
@@ -1,54 +1,53 @@
 // Queue class by defining the constructor
 function Queue() {
-    this.dataStore = [];
-    this.enqueue = enqueue;
-    this.dequeue = dequeue;
-    this.front = front;
-    this.back = back;
-    this.toString = toString;
-    this.empty = empty;
-    }
-    // adding an element
-    function enqueue(element) {
-        this.dataStore.push(element);
-        }
-        // removes an element
-        function dequeue() {
-            return this.dataStore.shift();
-        }
+  this.dataStore = [];
+  this.enqueue = enqueue;
+  this.dequeue = dequeue;
+  this.front = front;
+  this.back = back;
+  this.toString = toString;
+  this.empty = empty;
+}
+// adding an element
+function enqueue(element) {
+  this.dataStore.push(element);
+}
+// removes an element
+function dequeue() {
+  return this.dataStore.shift();
+}
 
-        // getting front and back elements of a queue using these functions
+// getting front and back elements of a queue using these functions
 
-        function front() {
-            return this.dataStore[0];
-        }   
-        function back() {
-            return this.dataStore[this.dataStore.length-1];
-         }
-        // all the elements in a queue
-         function toString() {
-            var retStr = "";
-            for (var i = 0; i < this.dataStore.length; ++i) {
-            retStr += this.dataStore[i] + "\n";
-            }
-            return retStr;
-            }
-            //if a queue is empty
-            function empty() {
-                if (this.dataStore.length == 0) {
-                return true;
-                }
-                else {
-                return false;
-                }
-                }
-                // test programe 
+function front() {
+  return this.dataStore[0];
+}
+function back() {
+  return this.dataStore[this.dataStore.length - 1];
+}
+// all the elements in a queue
+function toString() {
+  var retStr = "";
+  for (var i = 0; i < this.dataStore.length; ++i) {
+    retStr += this.dataStore[i] + "\n";
+  }
+  return retStr;
+}
+//if a queue is empty
+function empty() {
+  if (this.dataStore.length == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+// test programe
 
-                var q = new Queue();
+var q = new Queue();
 q.enqueue("Alex");
 q.enqueue("Smith");
 q.enqueue("John");
-q.enqueue("Ahmed")
+q.enqueue("Ahmed");
 
 console.log(q.toString());
 q.dequeue();
