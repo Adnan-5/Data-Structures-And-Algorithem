8b3b37bbff2252973ba43054c1929e296f0290d0
Adnan-5 <sahilladnan810@gmail.com>
2022-07-26 18:23:55 +0500

    testing code

--- Queue/queue.js
+++ Queue/queue.js
@@ -43,3 +43,14 @@ function Queue() {
                 }
                 }
                 // test programe 
+
+                var q = new Queue();
+q.enqueue("Meredith");
+q.enqueue("Cynthia");
+q.enqueue("Jennifer");
+
+console.log(q.toString());
+q.dequeue();
+console.log(q.toString());
+console.log("Front of queue: " + q.front());
+console.log("Back of queue: " + q.back());
