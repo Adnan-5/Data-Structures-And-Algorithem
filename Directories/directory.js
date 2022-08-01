361e6183cc3c56fdebd0e0252d4373c032341063
Adnan-5 <sahilladnan810@gmail.com>
2022-08-01 17:00:22 +0500

    bugs fix code updated

--- Directories/directory.js
+++ Directories/directory.js
@@ -1,17 +1,23 @@
 // defining the Directory class here
-function Directory() {
+function Dictionary() {
     this.add = add;
     this.datastore = new Array();
     this.find = find;
     this.remove = remove;
     this.showAll = showAll;
-}
-function add(key, value) {
+    }
+    function add(key, value) {
     this.datastore[key] = value;
     }
     function find(key) {
-        return this.datastore[key];
-        }  
-   function remove(key){
+    return this.datastore[key];
+  }
+    function remove(key) {
     delete this.datastore[key];
-   }     
+    }
+    function showAll() {
+    for  (var key in Object.keys(this.datastore)) {
+    console.log(key + " -> " + this.datastore[key]);
+    }
+    }
+
