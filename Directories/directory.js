4eaea179a2e740f4123ce9efe6ce0236e06b5855
Adnan-5 <sahilladnan810@gmail.com>
2022-08-01 16:27:27 +0500

    define the directory class

--- Directories/directory.js
+++ Directories/directory.js
@@ -1,1 +1,8 @@
-//  defining the Directory class here
+// defining the Directory class here
+function Directory() {
+    this.add = add;
+    this.datastore = new Array();
+    this.find = find;
+    this.remove = remove;
+    this.showAll = showAll;
+}
